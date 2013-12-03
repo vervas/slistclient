13769dc521b6cc72f11fcae9738c1e117838d016
Dimitris Verraros <vervas.g@gmail.com>
2013-12-03 13:51:21 +0100

    New item form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 import requests, json
 app = Flask(__name__)
 
@@ -10,18 +10,25 @@ def hello():
     return redirect(url_for('login'))
 
 
-@app.route('/lists')
+@app.route('/lists', methods=['GET', 'POST'])
 def lists():
     response = requests.get(SERVICE_BASE_URL + 'lists')
     result = json.loads(response.content)
     return render_template('lists.jinja', lists=result.items())
 
 
-@app.route('/lists/<list_id>/items')
+@app.route('/lists/<list_id>/items', methods=['GET', 'POST'])
 def items(list_id):
     response = requests.get(SERVICE_BASE_URL + 'lists/' + list_id + '/items')
     result = json.loads(response.content)
     return render_template('items.jinja', items=result)
+
+
+@app.route('/lists/<list_id>/items/new')
+def new_item(list_id, methods=['GET', 'POST']):
+    if request.method == 'POST':
+        return request.form['name'] + " " + request.form['priority']
+    return render_template('new_item.jinja')
 
 
 @app.route('/profile/<user_id>')
