4a1deb34fc9daeedf00fafba5fac265b249fe931
Dimitris Verraros <vervas.g@gmail.com>
2013-12-03 19:40:00 +0100

    Complete

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, redirect, url_for, render_template, request
-import requests, json
+import requests
+import json
 app = Flask(__name__)
 
 
 SERVICE_BASE_URL = 'http://snf-24511.vm.okeanos.grnet.gr:5000/'
 
+
 @app.route("/")
 def hello():
-    return redirect(url_for('login'))
+    return redirect(url_for('lists'))
 
 
 @app.route('/lists', methods=['GET', 'POST'])
@@ -17,18 +19,33 @@ def lists():
     return render_template('lists.jinja', lists=result.items())
 
 
-@app.route('/lists/<list_id>/items', methods=['GET', 'POST'])
+@app.route('/lists/<list_id>/items')
 def items(list_id):
     response = requests.get(SERVICE_BASE_URL + 'lists/' + list_id + '/items')
     result = json.loads(response.content)
     return render_template('items.jinja', items=result)
 
 
-@app.route('/lists/<list_id>/items/new')
-def new_item(list_id, methods=['GET', 'POST']):
+@app.route('/lists/<list_id>/items/new', methods=['GET', 'POST'])
+def new_item(list_id):
     if request.method == 'POST':
-        return request.form['name'] + " " + request.form['priority']
+        payload = {'name': request.form['name'], 'priority': request.form['priority']}
+        response = requests.post(SERVICE_BASE_URL + 'lists/' + list_id + '/items', payload)
+        if response.status_code == 201:
+            return redirect('lists/{}/items'.format(list_id))
     return render_template('new_item.jinja')
+
+
+@app.route('/lists/<list_id>/items/<name>')
+def delete_item(list_id, name):
+    response = requests.delete(SERVICE_BASE_URL + 'lists/' + list_id + '/items/' + name)
+    return redirect('lists/{}/items'.format(list_id))
+
+
+@app.route('/lists/<list_id>')
+def delete_list(list_id):
+    response = requests.delete(SERVICE_BASE_URL + 'lists/' + list_id)
+    return redirect('lists')
 
 
 @app.route('/profile/<user_id>')
